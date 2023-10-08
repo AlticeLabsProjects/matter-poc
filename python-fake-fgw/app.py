11d9e936f6ff24c933ba4b04f759200ea19caaba
Fernando Afonso <fernando-j-afonso@alticelabs.com>
2023-10-08 11:01:04 +0100

    Serial Number added

--- python-fake-fgw/app.py
+++ python-fake-fgw/app.py
@@ -1,5 +1,22 @@
 import json
 from flask import Flask
+import os.path
+from getmac import get_mac_address
+
+
+def get_serial_number():
+    file_path = "/sys/firmware/devicetree/base/serial-number"
+
+    if os.path.isfile(file_path):
+        f = open(file_path, "r")
+
+        try:
+            return f.readline()
+        finally:
+            f.close()
+    else:
+        return "NOSERIAL"
+
 
 app = Flask(__name__)
 
@@ -11,10 +28,10 @@ def handle_request():
             {
                 "eqModel": "GR141DG",
                 "swVersion": "3GN8020700r1F",
-                "serialNumber": "5054494EA8DC0C7F",
-                "mac": "CC:19:A8:DC:0C:7F",
-                "smartMeshSupport": "1",
-                "smartMeshStatus": "1",
+                "serialNumber": get_serial_number(),
+                "mac": get_mac_address(),
+                "smartMeshSupport": "0",
+                "smartMeshStatus": "0",
                 "pad": None,
             }
         ),
