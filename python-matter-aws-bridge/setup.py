8b8c587d17aebe2d74fde5fa79970ebd84eb6425
Fernando Afonso <fernando-j-afonso@alticelabs.com>
2023-11-17 21:03:15 +0000

    Agora sim, a coisa bem feita com setup.py

---
+++ python-matter-aws-bridge/setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import find_packages, setup
+
+setup(
+    name="Matter AWS Bridge",
+    version="1.0",
+    packages=find_packages(where="src"),
+    entry_points={
+        "console_scripts": [
+            "matterawsbridge=src.main:main",
+        ]
+    },
+    install_requires=[
+        "aenum==3.1.15",
+        "awscrt==0.19.1",
+        "awsiotsdk==1.19.0",
+        "certifi==2023.7.22",
+        "cffi==1.16.0",
+        "charset-normalizer==3.3.0",
+        "cryptography==41.0.4",
+        "dacite==1.8.1",
+        "idna==3.4",
+        "pycparser==2.21",
+        "python-dotenv==1.0.0",
+        "requests==2.31.0",
+        "urllib3==2.0.6",
+        "websocket-client==1.6.4",
+        "chip-clusters",
+    ],
+    dependency_links=[
+        os.path.join(os.getcwd(), "deps", "chip_clusters-0.0-py3-none-any.whl")
+    ],
+)
